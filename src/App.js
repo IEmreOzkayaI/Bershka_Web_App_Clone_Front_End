cef37782aae5b2219dc7dac82f5d6cce1ee8c47a
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2023-03-01 02:21:26 +0300

    front end finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import Home from "./pages/Home";
+import { StateContext } from "./layouts/Context/StateContext";
+import Dashboard from "./pages/home/Dashboard";
+import Navbar from "./layouts/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <StateContext>
+        {/* <Home/> */}
+        <Navbar />
+        <Dashboard />
+        
+      </StateContext>
     </div>
   );
 }
